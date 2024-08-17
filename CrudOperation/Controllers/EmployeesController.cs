b6c3a267b18f4e4dfb06e8f5c490c206d31fc87e
sasatee <sarvam2601@gmail.com>
2024-08-17 12:32:53 +0400

    Add project files.

---
+++ CrudOperation/Controllers/EmployeesController.cs
@@ -0,0 +1,69 @@
+﻿using CrudOperation.Data;
+using CrudOperation.Models;
+using CrudOperation.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudOperation.Controllers
+{
+    // localhost:XXXX/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppicationDbContext dbContext;
+
+        public EmployeesController(AppicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployee = dbContext.Employees.ToList();
+
+            return Ok(allEmployee);
+
+
+        }
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employeeEntity = dbContext.Employees.Find(id);
+
+            if (employeeEntity is null)
+            {
+                return NotFound();
+            
+            }
+            return Ok(employeeEntity);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployees(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            var uri = new Uri($"{Request.Scheme}://{Request.Host}{Request.Path}/{employeeEntity.Id}");
+
+            return Created(uri, employeeEntity);
+
+
+        }
+
+
+
+      }
+    }
---
+++ CrudOperation/Data/AppicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudOperation.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperation.Data
+{
+    public class AppicationDbContext:DbContext
+    {
+        public AppicationDbContext(DbContextOptions<AppicationDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
